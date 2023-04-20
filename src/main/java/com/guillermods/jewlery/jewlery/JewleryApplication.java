5168b4961a8ab96803e8580586be12eba3d8b219
gdiazs <diazgbs@gmail.com>
2023-04-19 21:33:51 -0600

    first commit

---
+++ src/main/java/com/guillermods/jewlery/jewlery/JewleryApplication.java
@@ -0,0 +1,13 @@
+package com.guillermods.jewlery.jewlery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JewleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guillermods/jewlery/jewlery/JewleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guillermods.jewlery.jewlery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
