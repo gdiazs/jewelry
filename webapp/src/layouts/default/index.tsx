73a0e1007c0871e2aa86b57d89d994d44397a3ca
gdiazs <diazgbs@gmail.com>
2023-04-25 22:43:23 -0600

    Add bg

--- webapp/src/layouts/default/index.tsx
+++ webapp/src/layouts/default/index.tsx
@@ -17,7 +17,7 @@ const Main = (props: LayoutProps) => {
       <Nav />
       <div className="row mt-3 ">
         <div className="col md:col-8 md:col-offset-2">
-          <div className="shadow-2 m-2 p-2 border-round">
+          <div className="shadow-2 m-2 p-2 border-round bg-white">
             <Outlet />
           </div>
         </div>
