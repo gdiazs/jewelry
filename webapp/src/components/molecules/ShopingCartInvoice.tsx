8119d0d8c5de07854573bd34ae5956495c62a357
gdiazs <diazgbs@gmail.com>
2023-04-26 20:42:24 -0600

    Update

--- webapp/src/components/molecules/ShopingCartInvoice.tsx
+++ webapp/src/components/molecules/ShopingCartInvoice.tsx
@@ -1,15 +1,60 @@
 import { useSelector } from "react-redux/es/hooks/useSelector";
-import { productState } from "../../store/products/productsSlice";
+import { cleanInvoice, productState } from "../../store/products/productsSlice";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { Button } from "primereact/button";
 import { useNavigate } from "react-router-dom";
+import { Dialog } from "primereact/dialog";
+import Product from "../../store/products/Product";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../store";
+
+type ProductCounter = {
+  product: Product;
+  priceFormatted: string;
+  count: number;
+  subtotal: number;
+  subtotalFormatted: string;
+};
 
 function ShopingCartInvoice() {
+  const [visible, setVisible] = useState(false);
   const { cartItems }: any = useSelector(productState);
+  const dispatch = useDispatch<AppDispatch>();
+
   const browserNavigation = useNavigate();
 
-  const itemTemplate = () => {};
+  const colonScale = Intl.NumberFormat("cr-CR");
+  let invoiceTotals = 0;
+  const cartItemsMapped = new Map<string, ProductCounter>();
+
+  cartItems.forEach((product: Product) => {
+    const itemFound = cartItemsMapped.get(product.id);
+
+    if (itemFound) {
+      itemFound.count++;
+      itemFound.subtotal += product.price;
+      itemFound.subtotalFormatted = colonScale.format(itemFound.subtotal);
+    } else {
+      cartItemsMapped.set(product.id, {
+        product,
+        priceFormatted: colonScale.format(product.price),
+        count: 1,
+        subtotal: product.price,
+        subtotalFormatted: colonScale.format(product.price),
+      });
+    }
+  });
+
+  const productsInCart: Array<ProductCounter> = Array.from(
+    cartItemsMapped.values()
+  );
+
+  productsInCart.forEach((productInCart) => {
+    console.log(productInCart);
+    invoiceTotals += productInCart.subtotal;
+  });
 
   return (
     <>
@@ -23,11 +68,47 @@ function ShopingCartInvoice() {
         className="mb-2"
       />
 
-      <DataTable value={cartItems}>
-        <Column className="py-4" field="name" header="Producto"></Column>
-        <Column field="description" header="Descripcion"></Column>
-        <Column field="price" className="text-right" header="Precio"></Column>
+      <DataTable value={productsInCart}>
+        <Column
+          className="py-4"
+          field="product.name"
+          header="Producto"
+        ></Column>
+        <Column field="product.description" header="Descripcion"></Column>
+        <Column field="count" header="Cantidad"></Column>
+        <Column
+          field="priceFormatted"
+          className="text-right"
+          header="Precio"
+        ></Column>
+        <Column
+          field="subtotalFormatted"
+          className="text-right"
+          header="Sub Total"
+        ></Column>
       </DataTable>
+      <div className="text-right mt-2 mr-3 text-xl">
+        Total: ₡{colonScale.format(invoiceTotals)}
+      </div>
+
+      <div className="mt-6 text-right">
+        <Button
+          label="Realizar Pago"
+          size="large"
+          onClick={() => {
+            setVisible(true);
+            dispatch(cleanInvoice());
+          }}
+        />
+        <Dialog
+          header="Resultado"
+          visible={visible}
+          style={{ width: "10vw" }}
+          onHide={() => setVisible(false)}
+        >
+          <p className="m-0">Pago satisfactorio</p>
+        </Dialog>
+      </div>
     </>
   );
 }
--- webapp/src/store/products/productsSlice.ts
+++ webapp/src/store/products/productsSlice.ts
@@ -8,9 +8,20 @@ export interface ProductState {
   cartItems: Array<Product>;
 }
 
+const PRODUCTS_LOCAL_STORAGE = "jewlery:products";
+const CART_ITEMS_LOCAL_STORAGE = "jewlery:cartItems";
+
+const productsFromStrore: Array<Product> = JSON.parse(
+  localStorage.getItem(PRODUCTS_LOCAL_STORAGE) || "[]"
+);
+
+const cartItemsFromStore: Array<Product> = JSON.parse(
+  localStorage.getItem(CART_ITEMS_LOCAL_STORAGE) || "[]"
+);
+
 const initialState: ProductState = {
-  products: [],
-  cartItems: [],
+  products: productsFromStrore,
+  cartItems: cartItemsFromStore,
 };
 
 export const fetchProductsThunk = createAsyncThunk(
@@ -42,14 +53,45 @@ const productsSlice: any = createSlice({
         }
       });
 
+      localStorage.setItem(
+        PRODUCTS_LOCAL_STORAGE,
+        JSON.stringify(state.products)
+      );
+      localStorage.setItem(
+        CART_ITEMS_LOCAL_STORAGE,
+        JSON.stringify(state.cartItems)
+      );
+
       return state;
     },
+
+    cleanInvoice(state: ProductState){
+      console.log(state)
+      localStorage.removeItem(CART_ITEMS_LOCAL_STORAGE);
+      localStorage.removeItem(PRODUCTS_LOCAL_STORAGE);
+
+      state.cartItems = [];
+      state.products = [];
+
+      fetchProductsThunk();
+    }
   },
 
   extraReducers(builder) {
     builder.addCase(
       fetchProductsThunk.fulfilled,
       (state: ProductState, action) => {
+        const cartItemMap: Map<string, Product> = new Map(
+          state.cartItems.map((product: Product) => [product.id, product])
+        );
+
+        action.payload.forEach((product: Product) => {
+          const foundProduct = cartItemMap.get(product.id);
+          if (foundProduct) {
+            product.quantity = foundProduct.quantity;
+          }
+        });
+
         state.products = action.payload;
       }
     );
@@ -67,6 +109,9 @@ const productsSlice: any = createSlice({
   },
 });
 
+export function cartiItemsToMap(products: any): Map<string, Product> {
+  return new Map( products.map((product: Product) => [product.id, product]))
+};
 export const productState = (state: RootState) => state.productReducer;
-export const { removeItem, addItemToCart } = productsSlice.actions;
+export const { removeItem, addItemToCart, cleanInvoice } = productsSlice.actions;
 export default productsSlice.reducer;
